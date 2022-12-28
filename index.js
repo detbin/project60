a4d43c158ff611b4a2d32dd1eadf5c930eb3f398
detbin <116663162+detbin@users.noreply.github.com>
2022-12-28 14:20:23 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+let nombre='David'
+let apellido='Eskenazi'
+let estudiante=`${nombre} ${apellido}`
+console.log(estudiante)
+let estudianteMayus=estudiante.toUpperCase()
+console.log(estudianteMayus)
+let estudianteMinus=estudiante.toLowerCase()
+console.log(estudianteMinus)
+let longitud=estudiante.length
+console.log(longitud)
+let comienzo=estudiante[0]
+console.log(comienzo)
+console.log(estudiante[13])
+console.log(estudiante.replace(' ',''))
+let contiene=estudiante.includes('David')
+console.log(contiene)
